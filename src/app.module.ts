d3077414d6d99686d87f04593fa759021fda7969
Herael <b.rousval@hotmail.com>
2021-03-27 14:18:16 +0100

    create dog and cat module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Global, Module } from '@nestjs/common';
+import { CatsModule } from './cats/cats.module';
+import { DogsModule } from './dogs/dogs.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CatsModule, DogsModule],
+  
 })
 export class AppModule {}
