37934c0d623aa725f7707f763b990e9758cac946
alpheonix <arthurblanc98@gmail.com>
2021-04-03 16:19:49 +0200

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { AuthService } from './modules/auth/auth.service';
+//import { AuthService } from './modules/auth/auth.service';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
-    private authService: AuthService,
+    //private authService: AuthService,
   ) {}
 
   @Get()
