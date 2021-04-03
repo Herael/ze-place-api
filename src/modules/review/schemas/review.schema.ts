74297f8f6edc6f6bb3e4ef976a62cbd6c63e4f8c
alpheonix <arthurblanc98@gmail.com>
2021-04-03 15:23:31 +0200

    Update review.schema.ts

--- src/modules/review/schemas/review.schema.ts
+++ src/modules/review/schemas/review.schema.ts
@@ -1,5 +1,5 @@
 import * as mongoose from 'mongoose';
-import { CustomerSchema } from 'src/modules/customer/schemas/customer.schema';
+import { CustomerSchema } from '../../customer/schemas/customer.schema';
 
 export const ReviewSchema = new mongoose.Schema({
   from: CustomerSchema,
