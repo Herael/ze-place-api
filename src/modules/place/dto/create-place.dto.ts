2dc5fbe99dee179c2592656151349dd465ab6200
Herael <b.rousval@hotmail.com>
2021-04-10 17:33:30 +0200

    update validation decorators in Place and Review objects

--- src/modules/place/dto/create-place.dto.ts
+++ src/modules/place/dto/create-place.dto.ts
@@ -1,13 +1,41 @@
+import {
+  IsDefined,
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  Max,
+} from 'class-validator';
 import { Review } from 'src/modules/review/interfaces/review.interface';
 import { Image } from 'src/modules/types';
 
 export class CreatePlaceDTO {
+  @IsString()
+  @IsNotEmpty()
   readonly title: string;
+
+  @IsString()
+  @IsNotEmpty()
   readonly location: string;
+
+  @IsString()
+  @IsNotEmpty()
   readonly description: string;
+
+  @IsString()
+  @IsNotEmpty()
   readonly images: Array<Image>;
+
+  @IsNumber()
+  @IsPositive()
+  @Max(5)
   readonly rate: number;
+
+  @IsNumber()
+  @IsPositive()
   readonly price: number;
+
+  @IsDefined()
   readonly reviews: Array<Review>;
   readonly created_at: Date;
 }
--- src/modules/review/dto/create-review.dto.ts
+++ src/modules/review/dto/create-review.dto.ts
@@ -1,8 +1,23 @@
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  Max,
+} from 'class-validator';
 import { Customer } from 'src/modules/customer/interfaces/customer.interface';
 
 export class CreateReviewDTO {
+  @IsNotEmpty()
   readonly from: Customer;
+
+  @IsString()
   readonly text: string;
+
+  @IsNumber()
+  @IsPositive()
+  @Max(5)
   readonly rate: number;
+
   readonly created_at: Date;
 }
