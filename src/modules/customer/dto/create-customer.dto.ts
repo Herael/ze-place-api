b1c325a8ca7dea5925e396b914e37df6d88739d5
Herael <b.rousval@hotmail.com>
2021-04-10 17:14:27 +0200

    add validation decorators for DTO objects

--- src/modules/customer/dto/create-customer.dto.ts
+++ src/modules/customer/dto/create-customer.dto.ts
@@ -1,10 +1,32 @@
+import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
+
 export class CreateCustomerDTO {
+  @IsString()
+  @IsNotEmpty()
   readonly first_name: string;
+
+  @IsString()
+  @IsNotEmpty()
   readonly last_name: string;
+
+  @IsEmail()
   readonly email: string;
+
+  @IsNotEmpty()
+  @Length(2, 64)
   readonly password: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly phone: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly address: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly description: string;
+
   readonly created_at: Date;
 }
