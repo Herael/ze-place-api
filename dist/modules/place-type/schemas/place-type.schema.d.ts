9c55695da2d5ea8ce7be56d043c7d92e0aabe154
Tristan <tristan.luong@gmail.com>
2021-06-09 17:13:53 +0200

    Dist

--- dist/modules/place-type/schemas/place-type.schema.d.ts
+++ dist/modules/place-type/schemas/place-type.schema.d.ts
@@ -1,3 +1,3 @@
 import * as mongoose from 'mongoose';
-export declare const PlaceTypeSchema: mongoose.Schema<mongoose.Document<any, any>, mongoose.Model<any, any, any>, undefined>;
-export declare const PlaceFeaturesSchema: mongoose.Schema<mongoose.Document<any, any>, mongoose.Model<any, any, any>, undefined>;
+export declare const PlaceTypeSchema: mongoose.Schema<mongoose.Document<any, {}>, mongoose.Model<any, any>, undefined>;
+export declare const PlaceFeaturesSchema: mongoose.Schema<mongoose.Document<any, {}>, mongoose.Model<any, any>, undefined>;
