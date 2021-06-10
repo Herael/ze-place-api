5d8d110229835cb77302853990673cdcf1b41bbf
Herael <b.rousval@hotmail.com>
2021-06-10 19:25:32 +0200

    update booking array length

--- src/modules/place/place.service.ts
+++ src/modules/place/place.service.ts
@@ -49,13 +49,7 @@ export class PlaceService {
   }
 
   async bookPlace(userId: string, placeId: string, bookingDTO: BookingDTO) {
-    console.log('user id : ', userId);
-    console.log('place id : ', placeId);
-
-    console.log(await this.customerModel.findById(userId));
-
     const user = await this.customerModel.findById(userId);
-    console.log('user from mongo : ', user);
     const place = await this.findById(placeId);
     const booking = {
       userId: user._id,
@@ -64,8 +58,8 @@ export class PlaceService {
       avatar: user.avatar,
       ...bookingDTO,
     };
-    const index = place.bookings.push(booking as Booking);
-    user.bookings.push(place.bookings[index]._id);
+    place.bookings.push(booking as Booking);
+    user.bookings.push(place.bookings[place.bookings.length - 1]._id);
     user.save();
     place.save();
   }
