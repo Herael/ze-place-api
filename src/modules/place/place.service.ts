677b6d6d8791bfbd2ae1c76d67b125fb54360fa4
Herael <b.rousval@hotmail.com>
2021-07-04 17:55:50 +0200

    display only last places on main page screen

--- src/modules/place/place.service.ts
+++ src/modules/place/place.service.ts
@@ -28,7 +28,11 @@ export class PlaceService {
 
   async getAllPlaces(userId: string, limit?: number) {
     const user = await this.customerModel.findById(userId);
-    const places = await this.placeModel.find().limit(limit).exec();
+    const places = await this.placeModel
+      .find()
+      .sort({ created_at: -1 })
+      .limit(limit)
+      .exec();
     const formattedPlaces = places.map((place) => {
       place.isFavorite = Boolean(
         user.favorites.find((p) => p._id.toString() === place._id.toString()),
